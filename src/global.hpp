6f21d700aee804235e071311caf0a5373342bd66
Aman Gupta <amangupta@Amans-MacBook-Pro.local>
2021-04-26 18:07:59 +0530

    adding global,image,input,map

---
+++ src/global.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <math.h>
+#include <string>
+
+
+map<String,int> screen_specifications = {
+    "width":640,
+    "height":480,
+    "offset_x":480
+}
+
+map<String,int> block = {
+    "size" : 20,
+    "count_x" : 24,
+    "count_y" : 24
+}
+
+enum class game_mode {
+  single,
+  multiplayer
+};
+
+
+struct Point {
+  int x;
+  int y;
+
+  bool operator==(const Point &p) const noexcept {
+    return x == p.x && y == p.y;
+  }
+
+  bool operator!=(const Point &p) const noexcept {
+    return x != p.x || y != p.y;
+  }
+
+  Point operator+(const Point &p) const noexcept {
+    return Point{x + p.x, y + p.y};
+  }
+
+  void operator+=(const Point &p) noexcept {
+    x += p.x;
+    y += p.y;
+  }
+
+ public:
+  int distance(const Point &p) const noexcept {
+    return (int)math.sqrt( pow((x-p.x),2) + pow((y-p.y),2) );
+  }
+};
+
+
---
+++ src/image.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+map<string,int> images = {
+    "player1":0,
+    "player2":1,
+    "bg":2,
+    "bg_red":3,
+    "bg_green":4,
+    "bg_blue":5,
+    "food":6,
+    "food_counter":7,
+    "mon1":8,
+    "mon2":9,
+    "mon3":10,
+    "mon4":11,
+    "mon_run":12,
+    "plate":13,
+    "count":14
+};
+
+class ImageManager {
+    std::vector<SDL_Surface *> images_;
+    SDL_Renderer *renderer_;
+
+    void load(const char *path, char image_type) noexcept {
+        SDL_Surface *image = IMG_Load(path);
+        if (!image) {
+            std::cerr << "error: " << IMG_GetError() << '\n';
+            exit(EXIT_FAILURE);
+        }
+        images_[image_type] = image;
+    }
+
+    public:
+        ImageManager(SDL_Renderer *renderer) noexcept : renderer_(renderer) 
+        {
+            images_.reserve(images["count"]);
+
+            const int flag = IMG_INIT_PNG;
+            if ((IMG_Init(flag) & flag) != flag) {
+                std::cerr << "error: " << IMG_GetError() << '\n';
+                exit(EXIT_FAILURE);
+            }
+
+            load("./data/player1.png", images["player1"]);
+            load("./data/player2.png", images["player2"]);
+            load("./data/bg.png", images["bg"]);
+            load("./data/bg_red.png", images["bg_red"]);
+            load("./data/bg_green.png", images["bg_green"]);
+            load("./data/bg_blue.png", images["bg_blue"]);
+            load("./data/food.png", images["food"]);
+            load("./data/food_counter.png", images["food_counter"]);
+            load("./data/akabei.png", images["mon1"]);
+            load("./data/pinky.png", images["mon2"]);
+            load("./data/aosuke.png", images["mon3"]);
+            load("./data/guzuta.png", images["mon4"]);
+            load("./data/mon_run.png", images["mon_run"]);
+            load("./data/plate.png", images["plate"]);
+        }
+        SDL_Texture *get(char image_type) const noexcept {
+            return SDL_CreateTextureFromSurface(renderer_, images_[image_type]);
+        }
+
+        void render_copy(SDL_Texture &texture, const SDL_Rect &src,const SDL_Rect &dst) const noexcept {
+            SDL_RenderCopy(renderer_, &texture, &src, &dst);
+        }
+    ~ImageManager() noexcept { atexit(IMG_Quit); }
+};
